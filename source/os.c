862c1e69d5d6fc4d8e3a36e712d46a022ab8f178
quguiyou <quguiyou1076@qq.com>
2023-06-08 21:20:54 +0800

    开启时钟中断

--- source/os.c
+++ source/os.c
@@ -28,8 +28,43 @@ struct
     [KERNEL_CODE_SEG / 8] = {0xffff, 0x0000, 0x9a00, 0x00cf},
     [KERNEL_DATA_SEG / 8] = {0xffff, 0x0000, 0x9200, 0x00cf}};
 
+struct
+{
+    uint16_t offset_l, selector, attr, offset_h;
+} idt_table[256] __attribute__((aligned(8))) = {};
+
+void outb(uint8_t data, uint16_t port)
+{
+    __asm__ __volatile__("outb %[v], %[p]" ::[p] "d"(port), [v] "a"(data));
+}
+
+void timer_int(void);
+
 void os_init(void)
 {
+    outb(0x11, 0x20);
+    outb(0x11, 0xA0);
+    outb(0x20, 0x21);
+    outb(0x28, 0xA1);
+    outb(1 << 2, 0x21);
+    outb(2, 0xA1);
+
+    outb(0x1, 0x21);
+    outb(0x1, 0xA1);
+    outb(0xfe, 0x21);
+    outb(0xff, 0xa1);
+
+    // 配置中断频率
+    int tmo = 1193180 / 10;
+    outb(0x36, 0x43);
+    outb((uint8_t)tmo, 0x40);
+    outb(tmo >> 8, 0x40);
+
+    idt_table[0x20].offset_l = (unit32_t)timer_int & 0xFFFF;
+    idt_table[0x20].offset_h = (unit32_t)timer_int >> 16;
+    idt_table[0x20].selector = KERNEL_CODE_SEG;
+    idt_table[0x20].attr = 0x8E00;
+
     pg_dir[MAP_ADDR >> 22] = (unit32_t)page_table | PDE_P | PDE_W | PDE_U;
     page_table[(MAP_ADDR >> 12) & 0x3FF] = (unit32_t)map_phy_buffer | PDE_P | PDE_W | PDE_U;
 }

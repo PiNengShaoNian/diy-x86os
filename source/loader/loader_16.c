872b8e636fb7ab009173bcf26daab26710f0c3d7
quguiyou <quguiyou1076@qq.com>
2023-06-12 21:31:55 +0800

    利用内联汇编显示字符串

--- source/loader/loader_16.c
+++ source/loader/loader_16.c
@@ -1,7 +1,19 @@
 __asm__(".code16gcc");
 
+static void show_msg(const char *msg)
+{
+    char c;
+    while ((c = *msg++) != '\0')
+    {
+        __asm__ __volatile__("mov $0xe, %%ah\n\t"
+                             "mov %[ch], %%al\n\n"
+                             "int $0x10" ::[ch] "r"(c));
+    }
+}
+
 void loader_entry(void)
 {
+    show_msg("loading...\n\r");
     for (;;)
     {
     }

3473f885d9dcc0b6ee3685ad01423adad5ace8dc
quguiyou <quguiyou1076@qq.com>
2023-06-17 15:18:23 +0800

    捕获除0异常

--- source/kernel/cpu/irq.c
+++ source/kernel/cpu/irq.c
@@ -1,16 +1,32 @@
 #include "comm/cpu_instr.h"
 #include "cpu/cpu.h"
 #include "cpu/irq.h"
+#include "os_cfg.h"
 
 #define IDT_TABLE_NR 128
 
 static gate_desc_t idt_table[IDT_TABLE_NR];
 
+static void do_default_handler(const char *message)
+{
+    for (;;)
+        ;
+}
+
+void exception_handler_unknown(void);
+
+void do_handler_unknown(void)
+{
+    do_default_handler("unknown exception");
+}
+
 void irq_init(void)
 {
     for (int i = 0; i < IDT_TABLE_NR; i++)
     {
-        gate_desc_set(idt_table + i, 0, 0, 0);
+        gate_desc_set(idt_table + i, KERNEL_SELECTOR_CS,
+                      (uint32_t)exception_handler_unknown,
+                      GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_IDT);
     }
 
     lidt((uint32_t)idt_table, sizeof(idt_table));

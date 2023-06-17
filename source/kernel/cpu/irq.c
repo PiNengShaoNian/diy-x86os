9153ddd688396246c0ecaa121817faa3cb43da98
quguiyou <quguiyou1076@qq.com>
2023-06-17 16:24:08 +0800

    利用宏重用异常处理代码

--- source/kernel/cpu/irq.c
+++ source/kernel/cpu/irq.c
@@ -20,6 +20,11 @@ void do_handler_unknown(exception_frame_t *frame)
     do_default_handler(frame, "unknown exception");
 }
 
+void do_handler_divider(exception_frame_t *frame)
+{
+    do_default_handler(frame, "Divider exception");
+}
+
 void irq_init(void)
 {
     for (int i = 0; i < IDT_TABLE_NR; i++)
@@ -29,5 +34,17 @@ void irq_init(void)
                       GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_IDT);
     }
 
+    irq_install(IRQ0_DE, exception_handler_divider);
+
     lidt((uint32_t)idt_table, sizeof(idt_table));
+}
+
+int irq_install(int irq_num, irq_handler_t handler)
+{
+    if (irq_num >= IDT_TABLE_NR)
+        return -1;
+
+    gate_desc_set(idt_table + irq_num, KERNEL_SELECTOR_CS, (uint32_t)handler,
+                  GATE_P_PRESENT | GATE_DPL0 | GATE_TYPE_IDT);
+    return 0;
 }
--- source/kernel/include/cpu/irq.h
+++ source/kernel/include/cpu/irq.h
@@ -1,13 +1,40 @@
 #ifndef IRQ_H
 #define IRQ_H
 
+// 中断号码
+#define IRQ0_DE 0
+#define IRQ1_DB 1
+#define IRQ2_NMI 2
+#define IRQ3_BP 3
+#define IRQ4_OF 4
+#define IRQ5_BR 5
+#define IRQ6_UD 6
+#define IRQ7_NM 7
+#define IRQ8_DF 8
+#define IRQ10_TS 10
+#define IRQ11_NP 11
+#define IRQ12_SS 12
+#define IRQ13_GP 13
+#define IRQ14_PF 14
+#define IRQ16_MF 16
+#define IRQ17_AC 17
+#define IRQ18_MC 18
+#define IRQ19_XM 19
+#define IRQ20_VE 20
+
 typedef struct _exception_frame_t
 {
     uint32_t gs, fs, es, ds;
     uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    uint32_t num, error_code;
     uint32_t eip, cs, eflags;
 } exception_frame_t;
 
+typedef void (*irq_handler_t)(void);
+
 void irq_init(void);
+int irq_install(int irq_num, irq_handler_t handler);
+
+void exception_handler_divider(void);
 
 #endif

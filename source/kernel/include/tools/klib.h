8868e7351b9b2b3e66fcc947241cd8d31f78f2ad
quguiyou <quguiyou1076@qq.com>
2023-06-18 15:16:23 +0800

    字符串和内存工具函数实现

---
+++ source/kernel/include/tools/klib.h
@@ -0,0 +1,15 @@
+#ifndef KLIB_H
+#define KLIB_H
+
+#include "comm/types.h"
+
+void kernel_strcpy(char *dest, const char *src);
+void kernel_strncpy(char *dest, const char *src, int size);
+int kernel_strncmp(const char *s1, const char *s2, int size);
+int kernel_strlen(const char *str);
+
+void kernel_memcpy(void *dest, void *src, int size);
+void kernel_memset(void *dest, uint8_t v, int size);
+int kernel_memcmp(void *d1, void *d2, int size);
+
+#endif
---
+++ source/kernel/tools/klib.c
@@ -0,0 +1,100 @@
+#include "tools/klib.h"
+
+void kernel_strcpy(char *dest, const char *src)
+{
+    if (!dest || !src)
+        return;
+
+    while (*dest && *src)
+        *dest++ = *src++;
+
+    *dest = '\0';
+}
+
+void kernel_strncpy(char *dest, const char *src, int size)
+{
+    if (!dest || !src || !size)
+        return;
+
+    char *d = dest;
+    const char *s = src;
+
+    while ((size-- > 0) && (*s))
+        *d++ = *s++;
+
+    if (size == 0)
+        *(d - 1) = '\0';
+    else
+        *d = '\0';
+}
+
+int kernel_strlen(const char *str)
+{
+    if (str == (const char *)0)
+        return 0;
+
+    const char *c = str;
+
+    int len = 0;
+    while (*c++)
+        len++;
+
+    return len;
+}
+
+/**
+ * 比较两个字符串，最多比较size个字符
+ * 如果某一字符串提前比较完成，也算相同
+ */
+int kernel_strncmp(const char *s1, const char *s2, int size)
+{
+    if (!s1 || !s2)
+        return -1;
+
+    // 2023-3-18 这里size没有用到
+    while (*s1 && *s2 && (*s1 == *s2) && size)
+    {
+        s1++;
+        s2++;
+        size--;
+    }
+
+    return !((*s1 == '\0') || (*s2 == '\0') || (*s1 == *s2));
+}
+
+void kernel_memcpy(void *dest, void *src, int size)
+{
+    if (!dest || !src || !size)
+        return;
+
+    uint8_t *s = (uint8_t *)src;
+    uint8_t *d = (uint8_t *)dest;
+    while (size--)
+        *d++ = *s++;
+}
+
+void kernel_memset(void *dest, uint8_t v, int size)
+{
+    if (!dest || !size)
+        return;
+
+    uint8_t *d = (uint8_t *)dest;
+    while (size--)
+        *d++ = v;
+}
+
+int kernel_memcmp(void *d1, void *d2, int size)
+{
+    if (!d1 || !d2)
+        return 1;
+
+    uint8_t *p_d1 = (uint8_t *)d1;
+    uint8_t *p_d2 = (uint8_t *)d2;
+    while (size--)
+    {
+        if (*p_d1++ != *p_d2++)
+            return 1;
+    }
+
+    return 0;
+}

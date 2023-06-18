8e8f2ea410e0c1a03ac71e3ad8618beee6373616
quguiyou <quguiyou1076@qq.com>
2023-06-18 14:47:17 +0800

    实现基本的信息输出

--- script/qemu-debug-win.bat
+++ script/qemu-debug-win.bat
@@ -1,2 +1,2 @@
 @REM 适用于windows
-start qemu-system-i386  -m 128M -s -S  -drive file=disk1.vhd,index=0,media=disk,format=raw -drive file=disk2.vhd,index=1,media=disk,format=raw -d pcall,page,mmu,cpu_reset,guest_errors,page,trace:ps2_keyboard_set_translation
+start qemu-system-i386  -m 128M -s -S -serial stdio  -drive file=disk1.vhd,index=0,media=disk,format=raw -drive file=disk2.vhd,index=1,media=disk,format=raw -d pcall,page,mmu,cpu_reset,guest_errors,page,trace:ps2_keyboard_set_translation
--- source/kernel/tools/log.c
+++ source/kernel/tools/log.c
@@ -1,9 +1,30 @@
 #include "tools/log.h"
+#include "comm/cpu_instr.h"
+
+#define COM1_PORT 0x3F8
 
 void log_init(void)
 {
+    outb(COM1_PORT + 1, 0x00);
+    outb(COM1_PORT + 3, 0x80);
+    outb(COM1_PORT + 0, 0x3);
+    outb(COM1_PORT + 1, 0x00);
+    outb(COM1_PORT + 3, 0x03);
+    outb(COM1_PORT + 2, 0xc7);
+    outb(COM1_PORT + 4, 0x0F);
 }
 
 void log_printf(const char *fmt, ...)
 {
+    const char *p = fmt;
+    while (*p != '\0')
+    {
+        while ((inb(COM1_PORT + 5) & (1 << 6)) == 0)
+            ;
+
+        outb(COM1_PORT, *p++);
+    }
+
+    outb(COM1_PORT, '\r');
+    outb(COM1_PORT, '\n');
 }

1c6a3a99d3bc8c9b46cf6737fd2f426f720360aa
quguiyou <quguiyou1076@qq.com>
2023-06-23 17:33:55 +0800

    创建内核页表

--- source/kernel/core/memory.c
+++ source/kernel/core/memory.c
@@ -1,8 +1,11 @@
 #include "core/memory.h"
 #include "tools/log.h"
 #include "tools/klib.h"
+#include "cpu/mmu.h"
 
 static addr_alloc_t paddr_alloc;
+
+static pde_t kernel_page_dir[PDE_CNT] __attribute__((aligned(MEM_PAGE_SIZE)));
 
 static void addr_alloc_init(addr_alloc_t *alloc, uint8_t *bits, uint32_t start,
                             uint32_t size, uint32_t page_size)
@@ -58,6 +61,95 @@ static uint32_t total_mem_size(boot_info_t *boot_info)
     return mem_size;
 }
 
+pte_t *find_pte(pde_t *page_dir, uint32_t vaddr, int alloc)
+{
+    pte_t *page_table;
+    pde_t *pde = page_dir + pde_index(vaddr);
+
+    if (pde->present)
+    {
+        page_table = (pte_t *)pde_paddr(pde);
+    }
+    else
+    {
+        if (alloc == 0)
+            return (pte_t *)0;
+
+        uint32_t pg_paddr = addr_alloc_page(&paddr_alloc, 1);
+        if (pg_paddr == 0)
+            return (pte_t *)0;
+
+        pde->v = pg_paddr | PDE_P | PDE_W | PDE_U;
+
+        page_table = (pte_t *)pg_paddr;
+        kernel_memset(page_table, 0, MEM_PAGE_SIZE);
+    }
+
+    return page_table + pte_index(vaddr);
+}
+
+int memory_create_map(pde_t *page_dir, uint32_t vaddr, uint32_t paddr,
+                      int count, uint32_t perm)
+{
+    for (int i = 0; i < count; i++)
+    {
+        // log_printf("create map: v-0x%x, p-0x%x, perm: 0x%x", vaddr, paddr, perm);
+        pte_t *pte = find_pte(page_dir, vaddr, 1);
+        if (pte == (pte_t *)0)
+        {
+            // log_printf("create pte failed. pte == 0");
+            return -1;
+        }
+
+        // log_printf("pte addr: 0x%x", (uint32_t)pte);
+        ASSERT(pte->present == 0);
+        pte->v = paddr | perm | PTE_P;
+
+        vaddr += MEM_PAGE_SIZE;
+        paddr += MEM_PAGE_SIZE;
+    }
+}
+
+void create_kernel_table(void)
+{
+    extern uint8_t s_text[], e_text[], s_data[];
+    extern uint8_t kernel_base[];
+
+    static memory_map_t kernel_map[] = {
+        {
+            kernel_base,
+            s_text,
+            kernel_base,
+            PTE_W,
+        },
+        {
+            s_text,
+            e_text,
+            s_text,
+            0,
+        },
+        {
+            s_data,
+            (void *)MEM_EBDA_START,
+            s_data,
+            PTE_W,
+        },
+    };
+
+    for (int i = 0; i < sizeof(kernel_map) / sizeof(memory_map_t); i++)
+    {
+        memory_map_t *map = &kernel_map[i];
+
+        uint32_t vstart = down2((uint32_t)map->vstart, MEM_PAGE_SIZE);
+        uint32_t vend = up2((uint32_t)map->vend, MEM_PAGE_SIZE);
+        uint32_t paddr = down2((uint32_t)map->pstart, MEM_PAGE_SIZE);
+        int page_count = (vend - vstart) / MEM_PAGE_SIZE;
+
+        memory_create_map(kernel_page_dir, vstart,
+                          paddr, page_count, map->perm);
+    }
+}
+
 void memory_init(boot_info_t *boot_info)
 {
     extern uint8_t *mem_free_start;
@@ -75,4 +167,7 @@ void memory_init(boot_info_t *boot_info)
     mem_free += bitmap_byte_count(paddr_alloc.size / MEM_PAGE_SIZE);
 
     ASSERT(mem_free < (uint8_t *)MEM_EBDA_START);
+
+    create_kernel_table();
+    mmu_set_page_dir((uint32_t)kernel_page_dir);
 }
---
+++ source/kernel/include/cpu/mmu.h
@@ -0,0 +1,77 @@
+#ifndef MMU_H
+#define MMU_H
+
+#include "comm/types.h"
+#include "comm/cpu_instr.h"
+
+#define PDE_CNT 1024
+
+#define PDE_P (1 << 0)
+#define PTE_P (1 << 0)
+#define PDE_W (1 << 1)
+#define PTE_W (1 << 1)
+#define PDE_U (1 << 2)
+
+typedef union _pde_t
+{
+    uint32_t v;
+    struct
+    {
+        uint32_t present : 1;
+        uint32_t write_enable : 1;
+        uint32_t user_mode_acc : 1;
+        uint32_t write_through : 1;
+        uint32_t cache_disable : 1;
+        uint32_t accessed : 1;
+        uint32_t : 1;
+        uint32_t ps : 1;
+        uint32_t : 4;
+        uint32_t phy_pt_addr : 20;
+    };
+} pde_t;
+
+typedef union _pte_t
+{
+    uint32_t v;
+    struct
+    {
+        uint32_t present : 1;
+        uint32_t write_enable : 1;
+        uint32_t user_mode_acc : 1;
+        uint32_t write_through : 1;
+        uint32_t cache_disable : 1;
+        uint32_t accessed : 1;
+        uint32_t dirty : 1;
+        uint32_t pat : 1;
+        uint32_t global : 1;
+        uint32_t : 3;
+        uint32_t phy_page_addr : 20;
+    };
+} pte_t;
+
+static inline uint32_t pde_index(uint32_t vaddr)
+{
+    return (vaddr >> 22);
+}
+
+static inline uint32_t pte_index(uint32_t vaddr)
+{
+    return (vaddr >> 12) & 0x3FF;
+}
+
+static inline uint32_t pde_paddr(pde_t *pde)
+{
+    return pde->phy_pt_addr << 12;
+}
+
+static inline uint32_t pte_paddr(pte_t *pte)
+{
+    return pte->phy_page_addr << 12;
+}
+
+static inline void mmu_set_page_dir(uint32_t paddr)
+{
+    write_cr3(paddr);
+}
+
+#endif

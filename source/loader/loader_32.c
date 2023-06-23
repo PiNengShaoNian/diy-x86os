d153ae8ceccfaea06091bf75f02376ed7624c64f
quguiyou <quguiyou1076@qq.com>
2023-06-23 15:37:54 +0800

    开启内存分页机制

--- source/loader/loader_32.c
+++ source/loader/loader_32.c
@@ -67,6 +67,37 @@ static void die(int code)
         ;
 }
 
+/**
+ * @brief 开启分页机制
+ * 将0-4M空间映射到0-4M和SYS_KERNEL_BASE_ADDR~+4MB空间
+ * 0-4MB的映射主要用于保护loader自己还能正常工作
+ * SYS_KERNEL_BASE_ADDR+4MB则用于为内核提供正确的虚拟地址空间
+ */
+void enable_page_mode(void)
+{
+#define PDE_P (1 << 0)
+#define PDE_PS (1 << 7)
+#define PDE_W (1 << 1)
+#define CR4_PSE (1 << 4)
+#define CR0_PG (1 << 31)
+
+    // 使用4MB页块，这样构造页表就简单很多，只需要1个表即可。
+    // 以下表为临时使用，用于帮助内核正常运行，在内核运行起来之后，将重新设置
+    static uint32_t page_dir[1024] __attribute__((aligned(4096))) = {
+        [0] = PDE_P | PDE_PS | PDE_W, // PDE_PS，开启4MB的页
+    };
+
+    // 设置PSE，以便启用4M的页，而不是4KB
+    uint32_t cr4 = read_cr4();
+    write_cr4(cr4 | CR4_PSE);
+
+    // 设置页表地址
+    write_cr3((uint32_t)page_dir);
+
+    // 开启分页机制
+    write_cr0(read_cr0() | CR0_PG);
+}
+
 extern boot_info_t boot_info;
 void load_kernel()
 {
@@ -76,6 +107,8 @@ void load_kernel()
 
     if (kernel_entry == 0)
         die(-1);
+
+    enable_page_mode();
 
     ((void (*)(boot_info_t *))kernel_entry)(&boot_info);
 }

e4c73eb8d882cef1ee051db7d751ca6e45b0d710
quguiyou <quguiyou1076@qq.com>
2023-07-03 17:26:09 +0800

    借助按键映射表进行键值转换

--- source/kernel/dev/kbd.c
+++ source/kernel/dev/kbd.c
@@ -2,11 +2,112 @@
 #include "cpu/irq.h"
 #include "comm/cpu_instr.h"
 #include "tools/log.h"
+#include "tools/klib.h"
+
+static kbd_state_t kbd_stat;
+
+/**
+ * 键盘映射表，分3类
+ * normal是没有shift键按下，或者没有numlock按下时默认的键值
+ * func是按下shift或者numlock按下时的键值
+ * esc是以esc开头的的键值
+ */
+static const key_map_t map_table[256] = {
+    [0x2] = {'1', '!'},
+    [0x3] = {'2', '@'},
+    [0x4] = {'3', '#'},
+    [0x5] = {'4', '$'},
+    [0x6] = {'5', '%'},
+    [0x7] = {'6', '^'},
+    [0x08] = {'7', '&'},
+    [0x09] = {'8', '*'},
+    [0x0A] = {'9', '('},
+    [0x0B] = {'0', ')'},
+    [0x0C] = {'-', '_'},
+    [0x0D] = {'=', '+'},
+    [0x0E] = {'\b', '\b'},
+    [0x0F] = {'\t', '\t'},
+    [0x10] = {'q', 'Q'},
+    [0x11] = {'w', 'W'},
+    [0x12] = {'e', 'E'},
+    [0x13] = {'r', 'R'},
+    [0x14] = {'t', 'T'},
+    [0x15] = {'y', 'Y'},
+    [0x16] = {'u', 'U'},
+    [0x17] = {'i', 'I'},
+    [0x18] = {'o', 'O'},
+    [0x19] = {'p', 'P'},
+    [0x1A] = {'[', '{'},
+    [0x1B] = {']', '}'},
+    [0x1C] = {'\n', '\n'},
+    [0x1E] = {'a', 'A'},
+    [0x1F] = {'s', 'B'},
+    [0x20] = {'d', 'D'},
+    [0x21] = {'f', 'F'},
+    [0x22] = {'g', 'G'},
+    [0x23] = {'h', 'H'},
+    [0x24] = {'j', 'J'},
+    [0x25] = {'k', 'K'},
+    [0x26] = {'l', 'L'},
+    [0x27] = {';', ':'},
+    [0x28] = {'\'', '"'},
+    [0x29] = {'`', '~'},
+    [0x2B] = {'\\', '|'},
+    [0x2C] = {'z', 'Z'},
+    [0x2D] = {'x', 'X'},
+    [0x2E] = {'c', 'C'},
+    [0x2F] = {'v', 'V'},
+    [0x30] = {'b', 'B'},
+    [0x31] = {'n', 'N'},
+    [0x32] = {'m', 'M'},
+    [0x33] = {',', '<'},
+    [0x34] = {'.', '>'},
+    [0x35] = {'/', '?'},
+    [0x39] = {' ', ' '},
+};
 
 void kbd_init(void)
 {
+    kernel_memset(&kbd_stat, 0, sizeof(kbd_stat));
     irq_install(IRQ1_KEYBOARD, (irq_handler_t)exception_handler_kbd);
     irq_enable(IRQ1_KEYBOARD);
+}
+
+static inline int is_make_code(uint8_t key_code)
+{
+    return !(key_code & 0x80);
+}
+
+static inline char get_key(uint8_t key_code)
+{
+    return key_code & 0x7f;
+}
+
+static void do_normal_key(uint8_t raw_code)
+{
+    char key = get_key(raw_code);
+    int is_make = is_make_code(raw_code);
+
+    switch (key)
+    {
+    case KEY_RSHIFT:
+        kbd_stat.rshift_press = is_make ? 1 : 0;
+        break;
+    case KEY_LSHFIT:
+        kbd_stat.lshift_press = is_make ? 1 : 0;
+        break;
+    default:
+        if (is_make)
+        {
+            if (kbd_stat.rshift_press || kbd_stat.lshift_press)
+                key = map_table[key].func;
+            else
+                key = map_table[key].normal;
+
+            log_printf("key %c", key);
+        }
+        break;
+    }
 }
 
 void do_handler_kbd(exception_frame_t *frame)
@@ -19,6 +120,8 @@ void do_handler_kbd(exception_frame_t *frame)
     }
 
     uint8_t raw_code = inb(KBD_PORT_DATA);
-    log_printf("key: %d", raw_code);
+
+    do_normal_key(raw_code);
+
     pic_send_eoi(IRQ1_KEYBOARD);
 }
--- source/kernel/include/dev/kbd.h
+++ source/kernel/include/dev/kbd.h
@@ -1,6 +1,23 @@
 #ifndef KBD_H
 #define KBD_H
 
+#include "comm/types.h"
+
+#define KEY_RSHIFT 0x36
+#define KEY_LSHFIT 0x2A
+
+typedef struct _key_map_t
+{
+    uint8_t normal;
+    uint8_t func;
+} key_map_t;
+
+typedef struct _kbd_state_t
+{
+    int lshift_press : 1;
+    int rshift_press : 1;
+} kbd_state_t;
+
 #define KBD_PORT_DATA 0x60
 #define KBD_PORT_STAT 0x64
 #define KBD_PORT_CMD 0x64

1d5cc9913a259bb6783ee7375ebc573213f6b4a3
quguiyou <quguiyou1076@qq.com>
2023-08-12 20:20:41 +0800

    windows网络环境搭建

--- script/qemu-debug-win.bat
+++ script/qemu-debug-win.bat
@@ -1,2 +1,2 @@
 @REM 适用于windows
-start qemu-system-i386  -m 128M -s -S -serial stdio  -drive file=disk1.vhd,index=0,media=disk,format=raw -drive file=disk2.vhd,index=1,media=disk,format=raw -d pcall,page,mmu,cpu_reset,guest_errors,page,trace:ps2_keyboard_set_translation
+start qemu-system-i386  -m 128M -s -S -netdev tap,id=mynet0,ifname=tap -device rtl8139,netdev=mynet0,mac=52:54:00:c9:18:27 -serial stdio  -drive file=disk1.vhd,index=0,media=disk,format=raw -drive file=disk2.vhd,index=1,media=disk,format=raw -d pcall,page,mmu,cpu_reset,guest_errors,page,trace:ps2_keyboard_set_translation
